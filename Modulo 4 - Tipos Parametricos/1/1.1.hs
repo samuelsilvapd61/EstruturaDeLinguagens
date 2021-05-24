4766c5058d01214c8384e52b0233da2211297503
Samuel de Oliveira Silva <48977392+samuelsilvapd61@users.noreply.github.com>
2021-05-24 17:56:03 -0300

    Add files via upload

--- Modulo 4 - Tipos Parametricos/1/1.1.hs
+++ Modulo 4 - Tipos Parametricos/1/1.1.hs
@@ -1,5 +1,10 @@
+-- Cada personagem tem Altura e Idade
 data Base = Base Double Int
 
+-- O personagem e:
+-- Um Guerreiro que tem forca
+-- Um Mago que tem magia
+-- Um Sacerdote que tem resistencia
 data Personagem = Guerreiro Base Int | Mago Base Double | Sacerdote Base Int
 
 p1 :: Personagem
