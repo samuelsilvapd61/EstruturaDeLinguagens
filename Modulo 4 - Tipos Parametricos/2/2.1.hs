4260c7ebb041dff7e02669a97810ae82b16b1f30
Samuel de Oliveira Silva <48977392+samuelsilvapd61@users.noreply.github.com>
2021-05-24 22:09:38 -0300

    Add files via upload

--- Modulo 4 - Tipos Parametricos/2/2.1.hs
+++ Modulo 4 - Tipos Parametricos/2/2.1.hs
@@ -1,8 +1,10 @@
 data ListaInt = NoInt Int (ListaInt) | VazioInt
+    deriving Show
 
 data Lista a = No a (Lista a) | Vazio
+    deriving Show
 
 l :: Lista ListaInt
 l = No (NoInt 10 VazioInt) (No (NoInt 30 VazioInt) Vazio)
 
-main = putStrLn "OK"
+main = print(l)
--- Modulo 4 - Tipos Parametricos/2/2.2.hs
+++ Modulo 4 - Tipos Parametricos/2/2.2.hs
@@ -1,6 +1,8 @@
 data ListaInt = NoInt Int (ListaInt) | VazioInt
+    deriving Show
 
 data Arvore a = Galho (Arvore a) a (Arvore a) | Folha
+    deriving Show
 
 lb :: Arvore Bool
 lb = Galho (Galho (Galho (Folha) False (Folha)) True (Folha)) True (Galho (Folha) True (Folha))
@@ -9,4 +11,4 @@ li :: Arvore ListaInt
 li = Galho (Folha) (NoInt 1 VazioInt) (Galho (Folha) (NoInt 2 VazioInt) (Folha))
 
 
-main = putStrLn "ok"
+main = print(lb, " - ", li)
